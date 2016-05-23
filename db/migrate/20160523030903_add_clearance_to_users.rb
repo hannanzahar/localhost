409615334d697fe78d4c003cc58f360e930bef4b
Sam Tingle <samueltingle@gmail.com>
2016-05-23 13:17:28 +0800

    added user login, facebook login, ominauth, clearance, figaro

--- db/migrate/20160523030903_add_clearance_to_users.rb
+++ db/migrate/20160523030903_add_clearance_to_users.rb
@@ -1,7 +1,7 @@
 class AddClearanceToUsers < ActiveRecord::Migration
   def self.up
     change_table :users do |t|
-      t.string :encrypted_password, limit: 128
+      t.string :encrypted_password, limit: 128, null: false
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128
     end
