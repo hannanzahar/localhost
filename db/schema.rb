120ba69c40657c19f3bfcbf50273798842a7062a
z1ho <ziho.89@gmail.com>
2016-05-25 11:56:39 +0800

    bootstrap work in progress

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160524081651) do
+ActiveRecord::Schema.define(version: 20160525030903) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20160524081651) do
     t.datetime "updated_at",                          null: false
     t.string   "avatar"
     t.date     "birthdate"
-    t.string   "location"
     t.string   "bio"
     t.string   "interests"
     t.string   "emer_contact"
@@ -72,6 +71,13 @@ ActiveRecord::Schema.define(version: 20160524081651) do
     t.string   "emer_address"
     t.string   "first_name"
     t.string   "last_name"
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
+    t.string   "street"
+    t.string   "city"
+    t.string   "state"
+    t.string   "coutry"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
