90f24ce1f116f5d2de4f642fdf25b6f4a56410e5
z1ho <ziho.89@gmail.com>
2016-05-26 15:58:45 +0800

    Modified CSS

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -59,7 +59,6 @@ class FriendshipsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_friendship
-      byebug
       @friendship = Friendship.find(params[:id])
     end
 
