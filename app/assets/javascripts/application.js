9f44ae2eeb366fd7385032776bef078de1362aab
hannanzahar <hannanzahar@outlook.com>
2016-05-26 17:31:36 +0800

    fixed edit/fixed map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,41 +15,13 @@
 //= require private_pub
 //= require chat
 //= require user
+//= require turbolinks
 //= require underscore
 //= require gmaps/google
-//= require turbolinks
-
+//= require_self
 //= require_tree .
 
-$(document).ready(function () {
-    //Initialize tooltips
-    $('.nav-tabs > li a[title]').tooltip();
     
-    //Wizard
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
-        var $target = $(e.target);
-    
-        if ($target.parent().hasClass('disabled')) {
-            return false;
-        }
-    });
-
-    $(".next-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
-
-    });
-    $(".prev-step").click(function (e) {
-
-        var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
-
-    });
-});
-
 function nextTab(elem) {
     $(elem).next().find('a[data-toggle="tab"]').click();
 }
