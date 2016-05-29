ed3d85b543d1429a61f56aa33dff6a22757290fb
hannanzahar <hannanzahar@outlook.com>
2016-05-29 17:09:06 +0800

    added pending friend requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,30 +41,34 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @user_friends = []
-    @user.friendships.each do |friendship|
+    @user.friendships.where(approved: true).each do |friendship|
       
       friend = User.find(friendship.friend_id)
       @user_friends << friend
     end
+    #user_friends is an array of all the approved friends
+    
     @user_friends = @user_friends.uniq
+
+    @requested_friends =[]
+    @user.friendships.where(approved: false).each do |friendship|
+      
+      friend = User.find(friendship.friend_id)
+      @requested_friends << friend
+    end
+    #requested friends is the friends that the current user has requested but they have not accepted
+    @requested_friends = @requested_friends.uniq
   
     @reviews = @user.reviews
     @review = Review.new
     @conversations = Conversation.involving(current_user).order("created_at DESC")
-    # byebug
-    # @friend_locs.each do |f|
-    #   friend = @user.friends[f]
-    # end
+  
 
     @hash = Gmaps4rails.build_markers(@user_friends) do |user, marker|
-          
         marker.lat user.latitude
         marker.lng user.longitude
         marker.infowindow [user.first_name, user.last_name].join(" ")
       end
-    # find friends- get their marker refer index
-    # @friends_locs = **
-    # @hash = blablabla (@friends_locs?)
   end
 
   def address
